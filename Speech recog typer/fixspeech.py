95dd726310a111c2a65c52cfbf526f51ea8b40e9
Alnas Kabeer <alnaskabeer@gmail.com>
2021-06-20 17:07:36 +0530

    learned something new

--- Speech recog typer/fixspeech.py
+++ Speech recog typer/fixspeech.py
@@ -1,3 +1,9 @@
-import speech_recognition as sr
+import pyttsx3
+import random
 
-print(sr.Microphone(1).list_microphone_names())
+engine = pyttsx3.init('sapi5')
+voice = engine.getProperty('voices')
+engine.setProperty('voice', voice[1].id)
+randomstring = random.choices('abcdefghijklmnopqrstuvwxyz', k=50000) 
+engine.say("".join(randomstring))
+engine.runAndWait()
